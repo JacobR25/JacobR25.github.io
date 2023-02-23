91f1d165c6082b7a58079e35a04f56e6039699d3
kellycouvrette <kellycouvrette@gmail.com>
2023-02-23 15:24:26 -0800

    Added Kelly.js
    Created a js file and some code to test out the new branch

---
+++ housing/src/Kelly.js
@@ -0,0 +1,13 @@
+function AppTester() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            my big test.
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default AppTester;
--- housing/src/index.js
+++ housing/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Kelly from './Kelly';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+const kelly = ReactDOM.createRoot(document.getElementById('Kelly'));
+kelly.render(
+  <React.StrictMode>
+    <Kelly />
   </React.StrictMode>
 );
 
