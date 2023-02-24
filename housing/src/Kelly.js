32a0de2b0f2d5b38fa86bbe1b468697ec3023e75
kellycouvrette <kellycouvrette@gmail.com>
2023-02-24 09:45:34 -0800

    Putting in the real Budget Categories
    It now has the proposed 8 categories, but percent is not customizable.

--- housing/src/Kelly.js
+++ housing/src/Kelly.js
@@ -52,13 +52,34 @@ function whatHouseCanIAfford(monthlyRentBudget){
 
 
 function makeChart(){
-    
+    // set the data
+    var rent = 1000; //later will be grabbed from what the user wants
+
+    //just doing budget to map, make it fully customizable
+    //here we will get the percentages that they want
+    var rentPercent = .10; //will actually get from input box
+    var transportationPercent = .10; //will actually get from input box
+    var foodPercent = .20;
+    var utilitiesPercent = .10;
+    var insurancePercent = .20;
+    var savingsPercent = .30;
+    var entertainmentPercent = .5;
+    var otherPercent = .5;
+    var monthlyIncome = 10000; //will actually come from client side
     return(
     <PieChart
         data={[
-            { title: 'One', value: 10, color: '#E38627' },
-            { title: 'Two', value: 15, color: '#C13C37' },
-            { title: 'Three', value: 20, color: '#6A2135' },
+            // { title: 'One', value:10, color: '#E38627'},
+            // { title: 'Two', value:15, color: '#F23427'},
+            // { title: 'Three', value:5, color: '#E38999'},
+            {title: "Rent", value: percentBudgetReturn(monthlyIncome, rentPercent), color: '#E38627'},
+            {title: "Transportation", value: percentBudgetReturn(monthlyIncome, transportationPercent), color: '#C38627'},
+            {title: "Food", value: percentBudgetReturn(monthlyIncome, foodPercent), color: '#D38627'},
+            {title: "Utilities", value: percentBudgetReturn(monthlyIncome, utilitiesPercent), color: '#E98627'},
+            {title: "Insurance", value: percentBudgetReturn(monthlyIncome, insurancePercent), color: '#D98627'},
+            {title: "Savings", value: percentBudgetReturn(monthlyIncome, savingsPercent), color: '#C38627'},
+            {title: "Entertainment", value: percentBudgetReturn(monthlyIncome, entertainmentPercent),color: '#E08627'},
+            {title: "Other", value: percentBudgetReturn(monthlyIncome, otherPercent), color: '#B98627'},
         ]}
     />);
 }
